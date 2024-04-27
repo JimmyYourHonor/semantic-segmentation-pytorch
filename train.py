25c2ac198ae07b5ab5caf5e3d15e2755dbc32b47
JimmyYourHonor <jif055@ucsd.edu>
2024-04-27 12:04:26 -0700

    Added eval after each epoch

--- train.py
+++ train.py
@@ -8,13 +8,97 @@ from distutils.version import LooseVersion
 # Numerical libs
 import torch
 import torch.nn as nn
+import numpy as np
+from scipy.io import loadmat
+from PIL import Image
 # Our libs
 from mit_semseg.config import cfg
-from mit_semseg.dataset import TrainDataset
+from mit_semseg.dataset import TrainDataset, ValDataset
 from mit_semseg.models import ModelBuilder, SegmentationModule
-from mit_semseg.utils import AverageMeter, parse_devices, setup_logger
-from mit_semseg.lib.nn import UserScatteredDataParallel, user_scattered_collate, patch_replication_callback, train_collate
+from mit_semseg.utils import AverageMeter, parse_devices, setup_logger, accuracy, intersectionAndUnion, colorEncode
+from mit_semseg.lib.nn import UserScatteredDataParallel, user_scattered_collate, patch_replication_callback, train_collate, async_copy_to
+from mit_semseg.lib.utils import as_numpy
 import matplotlib.pyplot as plt
+
+colors = loadmat('data/color150.mat')['colors']
+def visualize_result(data, pred, savepath):
+    (img, seg) = data
+
+    # segmentation
+    seg_color = colorEncode(seg, colors)
+
+    # prediction
+    pred_color = colorEncode(pred, colors)
+
+    # aggregate images and save
+    im_vis = np.concatenate((img, seg_color, pred_color),
+                            axis=1).astype(np.uint8)
+
+    Image.fromarray(im_vis).save(savepath)
+
+def evaluate(segmentation_module, loader, cfg, epoch, gpu=0):
+    acc_meter = AverageMeter()
+    intersection_meter = AverageMeter()
+    union_meter = AverageMeter()
+    time_meter = AverageMeter()
+
+    segmentation_module.eval()
+
+    for i, batch_data in enumerate(loader):
+        # process data
+        batch_data = batch_data[0]
+        seg_label = as_numpy(batch_data['seg_label'][0])
+        img_resized_list = batch_data['img_data']
+
+        torch.cuda.synchronize()
+        tic = time.perf_counter()
+        with torch.no_grad():
+            segSize = (seg_label.shape[0], seg_label.shape[1])
+            scores = torch.zeros(1, cfg.DATASET.num_class, segSize[0], segSize[1])
+            scores = async_copy_to(scores, gpu)
+
+            for img in img_resized_list:
+                feed_dict = batch_data.copy()
+                feed_dict['img_data'] = img
+                del feed_dict['img_ori']
+                del feed_dict['info']
+                feed_dict = async_copy_to(feed_dict, gpu)
+
+                # forward pass
+                scores_tmp = segmentation_module(feed_dict, segSize=segSize)
+                scores = scores + scores_tmp / len(cfg.DATASET.imgSizes)
+
+            _, pred = torch.max(scores, dim=1)
+            pred = as_numpy(pred.squeeze(0).cpu())
+
+        torch.cuda.synchronize()
+        time_meter.update(time.perf_counter() - tic)
+
+        # calculate accuracy
+        acc, pix = accuracy(pred, seg_label)
+        intersection, union = intersectionAndUnion(pred, seg_label, cfg.DATASET.num_class)
+        acc_meter.update(acc, pix)
+        intersection_meter.update(intersection)
+        union_meter.update(union)
+
+        # visualization
+        if i%200 == 0:
+            if not os.path.isdir(os.path.join(cfg.DIR, f"epoch_{epoch}")):
+                    os.makedirs(os.path.join(cfg.DIR, f"epoch_{epoch}"))
+            visualize_result(
+                (batch_data['img_ori'], seg_label),
+                pred,
+                os.path.join(cfg.DIR, f"epoch_{epoch}", f'val_{i}.png')
+            )
+    # summary
+    iou = intersection_meter.sum / (union_meter.sum + 1e-10)
+    for i, _iou in enumerate(iou):
+        print('class [{}], IoU: {:.4f}'.format(i, _iou))
+
+    print('[Eval Summary]:')
+    print('Mean IoU: {:.4f}, Accuracy: {:.2f}%, Inference Time: {:.4f}s'
+          .format(iou.mean(), acc_meter.average()*100, time_meter.average()))
+    
 
 # train one epoch
 def train(segmentation_module, loader, optimizers, ave_total_loss, history, epoch, cfg):
@@ -64,11 +148,11 @@ def train(segmentation_module, loader, optimizers, ave_total_loss, history, epoc
                           cfg.TRAIN.running_lr_encoder, cfg.TRAIN.running_lr_decoder,
                           acc.data.item()*100, miou.data.item()*100, ave_total_loss.average()))
 
-            fractional_epoch = epoch - 1 + 1. * i / len(loader)
-            history['train']['epoch'].append(fractional_epoch)
-            history['train']['loss'].append(loss.data.item())
-            history['train']['acc'].append(acc.data.item())
-            history['train']['miou'].append(miou.data.item())
+        fractional_epoch = epoch - 1 + 1. * i / len(loader)
+        history['train']['epoch'].append(fractional_epoch)
+        history['train']['loss'].append(loss.data.item())
+        history['train']['acc'].append(acc.data.item())
+        history['train']['miou'].append(miou.data.item())
 
 
 def checkpoint(nets, history, cfg, epoch, encoder_opt=None, decoder_opt=None):
@@ -194,6 +278,20 @@ def main(cfg):
         num_workers=cfg.TRAIN.workers,
         drop_last=True,
         pin_memory=True)
+    
+    # Dataset and Loader
+    dataset_val = ValDataset(
+        cfg.DATASET.root_dataset,
+        cfg.DATASET.list_val,
+        cfg.DATASET)
+    loader_val = torch.utils.data.DataLoader(
+        dataset_val,
+        batch_size=cfg.VAL.batch_size,
+        shuffle=False,
+        collate_fn=user_scattered_collate,
+        num_workers=5,
+        drop_last=True)
+    
     cfg.TRAIN.max_iters = len(loader_train) * cfg.TRAIN.num_epoch
     print('1 Epoch = {} iters'.format(len(loader_train)))
 
@@ -245,6 +343,15 @@ def main(cfg):
         plt.savefig(os.path.join(cfg.DIR ,'train_miou.png'), bbox_inches='tight')
         plt.clf()
         plt.close()
+
+        if (epoch+1) % 10 == 0:
+            # validation
+            segmentation_module.eval()
+            segmentation_module.decoder.use_softmax = True
+            evaluate(segmentation_module, loader_val, cfg, epoch)
+            segmentation_module.train()
+            segmentation_module.decoder.use_softmax = False
+            torch.cuda.empty_cache()
 
     print('Training Done!')
 
